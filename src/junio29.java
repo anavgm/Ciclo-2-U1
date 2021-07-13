6e8f5f674ef351ca04b4b16a2fef0cc40af197d8
anavgm <avgmendoza@gmail.com>
2021-07-13 17:25:23 -0500

    unidad 1

---
+++ src/junio29.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class junio29 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int ingreso = scanner.nextInt();
+        // String ingreso = scanner.nextLine();
+        System.out.println(ingreso);
+        
+    }
+    
+}
